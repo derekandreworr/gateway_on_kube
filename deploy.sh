d9b29a45773dec88f01bb6177d1bc8b18c80e8e7
geoff.r.duck@gmail.com <geoff.r.duck@gmail.com>
2019-04-24 14:36:07 -0700

    first commit of phase 2

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+echo INFO: Deploying Gateway and MySQL
+kubectl apply -f container-gateway-config.yml -f container-gateway-mysql.yml -f container-gateway.yml --validate=false
+sleep 5
+
+echo INFO: Deploying Live API Creator
+kubectl apply -f lac-nps.yml --validate=false
+sleep 5
+
+echo INFO: Exposing Gateway ports
+kubectl expose deployment gw-dc --type=NodePort
+
+echo   
+echo kubectl get service
+kubectl get service
+
+echo  
+echo kubectl get pods
+kubectl get pods
+
+#echo
+#echo gcloud compute instances list
+#gcloud compute instances list
+
+
+echo INFO: Please wait for Gateway to start...
+sleep 260
+curl -k -u admin:CAdemo123! -H "Content-Type: application/xml" -X POST -d @lac-policy.xml https://192.168.99.100:31843/restman/1.0/services
+
+sleep 5
+
+
+echo INFO: Calling the deployed gateway service to proxy to Live API Creator!
+curl -k -u admin:CAdemo123! https://192.168.99.100:31843/lac-policy
+
